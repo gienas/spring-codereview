21e2b941e7b86e6a721bcc88ec1d5b2ce1fdcc4d
Eugeniusz Neugebauer <eugeniusz.neugebauer@telia.no>
2017-06-24 10:18:56 +0200

    added-project

---
+++ spring-boot-rest-jpa/src/test/java/pl/ene/springbootrestjpa/SpringBootRestJpaApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.ene.springbootrestjpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootRestJpaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
